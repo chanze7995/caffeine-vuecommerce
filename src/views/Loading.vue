dcd45b2ee2ed2afb9fdffcb62db2552fefa2fe35
Chanze Wu <chanze7995@gmail.com>
2021-03-10 17:02:57 +0800

    complete Loading Page

--- src/views/Loading.vue
+++ src/views/Loading.vue
@@ -1,11 +1,25 @@
 <template>
 <div class="loadingContainer">
-  <div class="cupGroup">
-    <div class="cup">
-      <div class="coffee"></div>
-        <!-- <img src="@/assets/img/logoImg.svg" alt=""> -->
+  <div class="loadingPic">
+    <div class="cupGroup">
+      <div class="cup">
+        <div class="coffee"></div>
       </div>
-    <div class="cupHandle"></div>
+      <div class="cupHandle"></div>
+    </div>
+    <div class="plateGroup">
+      <div class="plate1"></div>
+      <div class="plate2"></div>
+    </div>
+  </div>
+  <div class="loadingText">
+    <div class="title">Brewing</div>
+    <div class="bounceBall">
+      <div class="ball"></div>
+      <div class="ball"></div>
+      <div class="ball"></div>
+      <div class="ball"></div>
+    </div>
   </div>
 </div>
 </template>
